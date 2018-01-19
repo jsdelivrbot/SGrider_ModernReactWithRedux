b9e080dea8d735b513b3e3e9573bdbd2a65ea4b6
unknown <psasa@gmail.com.com>
2018-01-19 14:20:53 +0100

    190118

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBmtlAmBdlxrlbQ6usnHrac9DDlA-mwEW4';
+
+
+
+
 
 // create new component. this component should produce
 // some html
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />    
-    </div>
-    );
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            videos: []
+        };
 
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+            }, (videos) => { 
+                    this.setState({ videos });
+        });
+        
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />    
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+
+}
 // take this components html and put it on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
